e1b1f6954b06c083176c1e351e012814466b872c
serhatbek <serhat.bekk@gmail.com>
2023-11-22 12:28:52 +0300

    detail page

--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -32,7 +32,7 @@ const ProductDetail = () => {
   const { id, title, price, category, description, image } = selectedProduct;
   if (isLoading) {
     return (
-      <div className='loading'>
+      <div className='ui grid container loading'>
         <div className='lds-ripple'>
           <div></div>
           <div></div>
@@ -41,7 +41,38 @@ const ProductDetail = () => {
     );
   }
 
-  return <div>Product Detail</div>;
+  return (
+    <section className='ui grid container'>
+      <div className='ui placeholder segment'>
+        <div className='ui two column stackable center aligned grid'>
+          <div className='ui vertical divider'>AND</div>
+          <div className='middle aligned row'>
+            <div className='column lp'>
+              <img className='ui fluid image' src={image} />
+            </div>
+            <div className='column rp'>
+              <h1>{title}</h1>
+              <h2>
+                <a className='ui teal tag label'>${price}</a>
+              </h2>
+              <h3 className='ui brown block header'>{category}</h3>
+              <p>{description}</p>
+              <div
+                className='ui vertical animated button'
+                tabIndex='0'
+                style={{ background: '#e38595', color: 'white' }}
+              >
+                <div className='hidden content'>
+                  <i className='shop icon'></i>
+                </div>
+                <div className='visible content'>Add to Cart</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default ProductDetail;
